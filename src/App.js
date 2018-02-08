374d7ed4fb62bb02254020721b499411c4db700c
Guilherme Toti <guilhermetoti@gmail.com>
2018-02-07 23:52:23 -0200

    #02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+class MyButton extends Component {
   render() {
     return (
-      <p>Hello, World, Teste!</p>
+      <button
+        onClick={() => { this.props.handleClick(this.props.label); }}
+      >
+        {this.props.label}
+      </button>
+    );
+  }
+}
+
+class MyLabel extends Component {
+  render() {
+    return <p>Cliquei no: {this.props.text}</p>
+  }
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labelText: '',
+    };
+  }
+
+  setLabelText = (labelText) => {
+    this.setState({ labelText });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MyLabel text={this.state.labelText} />
+        <MyButton handleClick={this.setLabelText} label="Botão 1" />
+        <MyButton handleClick={this.setLabelText} label="Botão 2" />
+        <MyButton handleClick={this.setLabelText} label="Botão 3" />
+        <MyButton handleClick={this.setLabelText} label="Botão 4" />
+      </div>
     );
   }
 }

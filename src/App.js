98517086b2bd2480aedfe1774bd1a856a0040418
Guilherme Toti <guilhermetoti@gmail.com>
2019-11-14 08:58:37 -0300

    #05

--- src/App.js
+++ src/App.js
@@ -1,48 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
-
-class MyButton extends Component {
-  render() {
-    return (
-      <button
-        onClick={() => { this.props.handleClick(this.props.label); }}
-      >
-        {this.props.label}
-      </button>
-    );
-  }
-}
-
-class MyLabel extends Component {
-  render() {
-    return <p>Cliquei no: {this.props.text}</p>
-  }
-}
-
 
 class App extends Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
-      labelText: '',
-    };
-
-    this.setLabelText = this.setLabelText.bind(this);
+      data: []
+    }
   }
 
-  setLabelText(labelText) {
-    this.setState({ labelText });
+  componentDidMount() {
+    axios.get('https://api.github.com/repositories')
+      .then(({ data }) => {
+        this.setState({ data })
+      })
   }
 
   render() {
+    const { data } = this.state
+
     return (
       <div className="App">
-        <MyLabel text={this.state.labelText} />
-        <MyButton handleClick={this.setLabelText} label="Botão 1" />
-        <MyButton handleClick={this.setLabelText} label="Botão 2" />
-        <MyButton handleClick={this.setLabelText} label="Botão 3" />
-        <MyButton handleClick={this.setLabelText} label="Botão 4" />
+        <ul>
+          {data.map(item => <li key={item.id}>{item.full_name}</li>)}
+        </ul>
       </div>
     );
   }
